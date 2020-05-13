6f8d4d22ffacb2a0859ca924da964a1e1d950e52
BY <byagihas@gmail.com>
2020-05-12 22:26:25 -0500

    Updated main view and added temperature functionality based on locale

--- src/App.js
+++ src/App.js
@@ -1,56 +1,61 @@
 import React, { Component } from 'react';
+import Markets from './Markets.js';
 import logo from './logo.svg';
 import './App.css';
 
-const { DateTime } = require('luxon')
-
+const { DateTime } = require('luxon');
+const cheerio = require('cheerio');
 class App extends Component {
 
   // Constructor
   constructor(props) {
     const time = new Date(Date.now()).toISOString();
     const localetime = new Date(Date.now()).toUTCString();
-
     const browsertime = Intl.DateTimeFormat().resolvedOptions().timeZone;
     const converted = DateTime.fromISO(time, { zone: browsertime });
 
-    let year = converted.c.year
+    let year = converted.c.year;
     let month = String(converted.c.month).length > 1 ? converted.c.month : '0' + converted.c.month;
     let day = String(converted.c.day).length > 1 ? converted.c.day : '0' + converted.c.day;
     let hour = String(converted.c.hour).length > 1 ? converted.c.hour : '0' + converted.c.hour;
     let minute = String(converted.c.minute).length > 1 ? converted.c.minute : '0' + converted.c.minute;
     let second = String(converted.c.second).length > 1 ? converted.c.second : '0' + converted.c.second;
-
-    let timeFormat = year  + '/' + month + '/' + day + ' ' + hour + ':' + minute + ':' + second
+    let timeFormat = year  + '/' + month + '/' + day + ' ' + hour + ':' + minute + ':' + second;
 
     super(props);
     this.state = {
-      currentBrowserTime: 'America/Chicago',
+      currentBrowserTime: "Loading...",
       currentDate: timeFormat,
       currentDateLocale: localetime,
-      urlTraced: null
+      urlTraced: null,
+      weather: null
     }
   }
   // compentDidMount
   // initialize time
-  componentDidMount(){
-    window.setInterval(function() {
-      /*
-      fetch("http://localhost:8080/")
+   componentDidMount(){
+      const browsertime = Intl.DateTimeFormat().resolvedOptions().timeZone;
+      let location = browsertime.substring(browsertime.indexOf("/"), browsertime.length + 1);
+      let weatherAPI = `https://api.weatherapi.com/v1/current.json?key=9923485ed49944f0a0e05530201305&q=${location}`;
+    // let weatherAPI = 'https://www.yahoo.com/news/weather/';
+      const requestOptions = {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+      };
+      
+      fetch(weatherAPI, requestOptions)
       .then(response => response.json())
       .then(json => {
         console.log(json);
-        this.setState({
-          currentDate: json.year  + '/' + json.month + '/' + json.day + ' ' + json.hour + ':' + json.minute + ':' + json.second
-        });
-      });*/
-      
+        this.setState({ weather: json.current.temp_f});
+      });
+
+    window.setInterval(function() {
       const time = new Date(Date.now()).toISOString();
       const localetime = new Date(Date.now()).toUTCString();
-  
       const browsertime = Intl.DateTimeFormat().resolvedOptions().timeZone;
       const converted = DateTime.fromISO(time, { zone: browsertime });
-  
+
       let year = converted.c.year
       let month = String(converted.c.month).length > 1 ? converted.c.month : '0' + converted.c.month;
       let day = String(converted.c.day).length > 1 ? converted.c.day : '0' + converted.c.day;
@@ -59,35 +64,39 @@ class App extends Component {
       let second = String(converted.c.second).length > 1 ? converted.c.second : '0' + converted.c.second;
 
       let timeFormat = year  + '/' + month + '/' + day + ' ' + hour + ':' + minute + ':' + second
-      
+    
       this.setState({
         currentBrowserTime: browsertime,
         currentDate: timeFormat,
         currentDateLocale: localetime
       });
-    }.bind(this), 1000)
+    }.bind(this), 1000);
   }
 
   render() {
     const { currentDate } = this.state;
     const { currentDateLocale } = this.state;
     const { currentBrowserTime } = this.state;
+    const { weather } = this.state;
     return (
       
       <div className="App">
         <header className="App-header">
-        <h2>Current time</h2>
           <table classname="App-table">
+            <tr><strong>Time/Weather: </strong></tr>
             <tr>
               <td>
                Current locale time({currentBrowserTime}): {currentDate}
               </td>
-              <br/>
             </tr>
-            <br/>
             <tr>
               <td>
                Current UTC time: {currentDateLocale}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Current temperature: {weather} F
               </td>
             </tr>
           </table>
---
+++ src/Markets.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+
+const { DateTime } = require('luxon');
+
+class Markets extends Component {
+
+    // Constructor
+    constructor(props) {
+        let price = '1';
+        super(props);
+        this.state = {
+            marketprices: price
+        }
+    }
+
+    // compentDidMount
+    componentDidMount(){
+        // Read markets
+    }
+
+    render() {
+        const marketprices = this.state.bind(marketprices);
+        return (
+        <div className="Markets">
+            <header className="Markets-header">
+                <h2>Current time</h2>
+                    <table classname="Markets-table">
+                        <tr>
+                            <td>
+                                Markets {marketprices}
+                            </td>
+                        </tr>
+                    </table>
+            </header>
+        </div>
+        );
+    }
+}
+
+export default Markets;
