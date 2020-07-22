1a61f33c5f543a2d1b3f7797d83bb23fab7da206
BY <byagihas@gmail.com>
2020-07-22 01:31:20 -0500

    Updated weather functionality to use nws api

--- src/App.js
+++ src/App.js
@@ -13,7 +13,9 @@ class App extends Component {
     const localetime = new Date(Date.now()).toUTCString();
     const browsertime = Intl.DateTimeFormat().resolvedOptions().timeZone;
     const converted = DateTime.fromISO(time, { zone: browsertime });
-
+    const latitude = '';
+    const longitutde = '';
+    
     let year = converted.c.year;
     let month = String(converted.c.month).length > 1 ? converted.c.month : '0' + converted.c.month;
     let day = String(converted.c.day).length > 1 ? converted.c.day : '0' + converted.c.day;
@@ -24,19 +26,23 @@ class App extends Component {
 
     super(props);
     this.state = {
+      zipcode: null,
       currentBrowserTime: "Loading..",
       currentDate: timeFormat,
       currentDateLocale: localetime,
       urlTraced: '',
       weather_current: "...",
-      weather_h : '...',
-      weather_l : '...',
+      short_forecast: '...',
+      detailed_forecast: '...',
       markets_dji_m: '...',
       markets_dji_p: '...',
       markets_nasdaq_m: '...',
       markets_nasdaq_p: '...',
       news_general: null,
-      news_timestamp: null
+      news_timestamp: null,
+      crypto_name: null,
+      crypto_price: null,
+      lat_long: null
     };
   };
   // compentDidMount
@@ -47,33 +53,46 @@ class App extends Component {
       let weathercode = '2379574';
 
       let encoding = 'UTF-8';
-      const headers = {
+      let headers = {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
         'Accept-Language' : 'en-US, en;q=0.5',
         'DNT' : '1',
         'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:76.0) Gecko/20100101 Firefox/76.0',
-        'X-Requested-With' : Math.random() // Sending random value for required CORS reroute
+        'X-Requested-With' : Math.random(),// Sending random value for required CORS reroute
+        'Access-Control-Allow-Origin' : '*'
       };
 
-      // getWeather
-      // Fetch and parse data for weather
-      const getWeather = function(code){
-          let weatherURL = `https://cors-anywhere.herokuapp.com/https://www.yahoo.com/news/weather/united-states/new-york/new-york-${code}`;
-          fetch(weatherURL, headers)
-          .then((response) => { 
-              return response.text();
-          }).then(html => {
-              const $ = cheerio.load(encoding_f.convert(html, encoding).toString('utf8'), encoding);
-              let string_t = JSON.stringify($('.now > span').first().contents().text()).replace('"','').replace('"','') + '°';
-              let string_h = JSON.stringify($('.high-low > span').first().contents().text()).replace('"','').replace('"','');
-              let string_l = JSON.stringify($('.high-low > span').last().contents().text()).replace('"','').replace('"','');
-
+      const getLatLong = function(){ 
+        navigator.geolocation.getCurrentPosition(
+            function(position){
               this.setState({ 
-                weather_current: string_t,
-                weather_h : string_h,
-                weather_l : string_l
+                lat_long: position.coords.latitude + "," + position.coords.longitude
               });
-          }).catch((err) => console.error(err));
+            }.bind(this)
+        );
+      }.bind(this);
+
+      const getWeatherNWS = function(latlong){
+        let weatherAPI = `https://api.weather.gov/points/${latlong}`;
+        fetch(weatherAPI, headers)
+        .then((response) => { 
+            return response.json();
+        }).then(json => {
+              let forecast = json.properties.forecast;
+              fetch(forecast, headers)
+              .then((response) => { 
+                  return response.json();
+              }).then(json => {
+                  let temp = json.properties.periods[0].temperature;
+                  let short_forecast = json.properties.periods[0].shortForecast;
+                  let detailed_forecast = json.properties.periods[0].detailedForecast;
+                  this.setState({ 
+                    weather_current: temp,
+                    short_forecast: short_forecast,
+                    detailed_forecast: detailed_forecast
+                  });
+              }).catch((err) => console.error(err));
+        }).catch((err) => console.error(err));
       }.bind(this);
 
       // getMarkets
@@ -96,15 +115,60 @@ class App extends Component {
                 return formatted + '|';
               }
             }).get().join('').split(' ').join(''));
+            let string_full_name = JSON.stringify($('.symbol').contents().map(function(){
+              let formatted = $(this).text().replace('\n', ' ').replace('\\s','').replace(' ','').trim();
+              if(formatted != ''){
+                return formatted + '|';
+              }
+            }).get().join('').split('"').join('').split(' ').join(''));
 
-            let string_formatted_m = string_full_m.split("|");
-            let string_formatted_p = string_full_p.split("|");
+            let string_formatted_m = string_full_m.replace('\"','').split("|");
+            let string_formatted_p = string_full_p.replace('\"','').split("|");
+            let string_formatted_name = string_full_name.replace('\"','').split("|");
 
-            this.setState({ 
-              markets_dji_m: '$' + string_formatted_m[0], 
-              markets_dji_p: string_formatted_p[0],
-              markets_nasdaq_m: '$' + string_formatted_m[2],
-              markets_nasdaq_p: string_formatted_p[2],
+            const markets = (arr1, arr2, arr3) => {
+              let limit = 6;
+              return (
+                arr1.map((item, index) => (
+                  index < limit ? <li key={index}>{arr3[index]}: {item} - {arr2[index]}</li> : ''
+                ))
+              )
+            }
+            this.setState({ markets_dji_m: markets(string_formatted_m, string_formatted_p, string_formatted_name) });
+        }).catch((err) => console.error(err));
+      }.bind(this);
+      // Crypto
+      const getCrypto = function(){
+        fetch('https://cors-anywhere.herokuapp.com/https://www.coinmarketcap.com/?=&=', headers)
+        .then((response) => { 
+            return response.text();
+        }).then(html => {
+            const $ = cheerio.load(encoding_f.convert(html, encoding).toString('utf8'), encoding);
+            let string_full_price = JSON.stringify($('.cmc-table__cell--sort-by__price > a').contents().map(function(){
+              let formatted = $(this).text().replace('\n', ' ').replace('\\s','').replace(' ','').trim();
+              if(formatted != ''){
+                return formatted + '|';
+              };
+            }).get().join('').split('"').join('').split(' ').join(''));
+            let string_full_name = JSON.stringify($('.cmc-table__cell--sort-by__name > div > a').contents().map(function(){
+              let formatted = $(this).text().replace('\n', ' ').replace('\\s','').replace(' ','').trim();
+              if(formatted != ''){
+                return formatted + '|';
+              }
+            }).get().join('').split('"').join('').split(' ').join(''));
+
+            let string_f_name = string_full_name.split('\"').join('').split('|');
+            let string_f_price = string_full_price.split('\"').join('').split('|');
+            const crypto = (arr1, arr2) => {
+              let limit = 6;
+              return (
+                arr1.map((item, index) => (
+                  index < limit ? <li key={index}>{index + 1}. {item} - {arr2[index]}</li> : ''
+                ))
+              )
+            }
+            this.setState({
+              crypto_name: crypto(string_f_name,string_f_price)
             });
         }).catch((err) => console.error(err));
       }.bind(this);
@@ -123,10 +187,11 @@ class App extends Component {
             let time_string = JSON.stringify($('.FeedItemMeta_date-updated').contents().map(function(){
               return $(this).text();
             }).get().join('|'));
+
             let news_formatted = news_string.split('\"').join('').split('|');
             let time_formatted = time_string.split('\"').join('').split('|');
             const news = (arr1, arr2) => {
-              let limit = 20;
+              let limit = 10;
               return (
                 arr1.map((item, index) => (
                   index < limit ? <li key={index}>{item} - {arr2[index]}</li> : ''
@@ -138,20 +203,22 @@ class App extends Component {
       }.bind(this);
 
       // Run data functions once
+      getLatLong();
+      //getWeather(weathercode);
       getMarkets();
-      getWeather(weathercode);
+      getCrypto();
       getNews();
 
       // Set spinner at 2 seconds to allow for loading.
       window.setTimeout(() => {
         document.getElementById('root').style.display = 'block';
         document.getElementById('spinner').style.display = 'none';
-      }, 2000);
+        getWeatherNWS(this.state.lat_long);
+      }, 3000);
 
       // Run data streams at intervals
       window.setInterval(function() {
-        getWeather(weathercode);
-      }.bind(this), 60000);
+      }.bind(this), 5000);
 
       window.setInterval(function() {
         getNews();
@@ -184,12 +251,12 @@ class App extends Component {
   }
 
   render() {
+    const { lat_long } = this.state;
     const { currentDate } = this.state;
     const { currentDateLocale } = this.state;
     const { currentBrowserTime } = this.state;
     const { weather_current } = this.state;
-    const { weather_h } = this.state;
-    const { weather_l } = this.state;
+    const { short_forecast } = this.state;
     const { markets_dji_m } = this.state;
     const { markets_dji_p } = this.state;
     const { markets_nasdaq_m } = this.state;
@@ -197,11 +264,9 @@ class App extends Component {
     const { headers } = this.state;
     const { news_general } = this.state;
     const { news_timestamp } = this.state;
-
-    let dji_percentage = 'neutral';
-    let nasdaq_percentage = 'neutral';
-    parseInt(markets_dji_p) < 0 ? dji_percentage = 'red' : dji_percentage = 'green';
-    parseInt(markets_nasdaq_p) < 0 ? nasdaq_percentage = 'red' : nasdaq_percentage = 'green';
+    const { crypto_name } = this.state;
+    const { crypto_price } = this.state;
+    const { detailed_forecast } = this.state;
 
    return (
       <div className="App">
@@ -211,17 +276,20 @@ class App extends Component {
                   <strong>QuBoard</strong>
               </th>
               <tbody>
+                <td>Lat/long: {lat_long}</td>
                 <td>
                   Locale time: ({currentBrowserTime}): {currentDate}
                 </td>
                 <td>
                   UTC time: {currentDateLocale}
                 </td>
+                <br/>
                 <td>
-                Current temperature: {weather_current.replace('"','').replace('"','')} F &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  Current temperature: {weather_current} F
                 </td>
-                <td>H: {weather_h.replace('"','').replace('"','')} F &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
-                <td>L: {weather_l.replace('"','').replace('"','')} F</td>
+                <td>
+                  Current forecast:<br/> {detailed_forecast}
+                </td>
               </tbody>
             </table>
           <br/>
@@ -229,15 +297,8 @@ class App extends Component {
             <th><strong>Markets</strong></th>
               <tbody>
                 <tr>
-                  <td>Dow Jones: {markets_dji_m.replace('"','').replace('"','')}&nbsp;&nbsp;
-                  <span style={{color: dji_percentage }}>{markets_dji_p.replace('"','').replace('"','')}</span>
-                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                  </td>
-                  <td>NASDAQ: {markets_nasdaq_m.replace('"','').replace('"','')}&nbsp;&nbsp;
-                  <span style={{color: nasdaq_percentage }}>{markets_nasdaq_p.replace('"','').replace('"','')}</span>
-                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                  </td>
-                  <td>S&P 500: </td>
+                  <td>General: <ul>{markets_dji_m}</ul></td>
+                  <td>Crypto: <ul>{crypto_name}</ul> </td>
                 </tr>
               </tbody>
           </table>
@@ -255,7 +316,7 @@ class App extends Component {
       </header>
       </div>
     );
-  } 
+  }
 }
 
 export default App;
